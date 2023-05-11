a5ab1fd9df8c5ba852b83b9b20eb5e7f5ebbed5a
Steve Yoo <steve@Steves-MacBook-Pro.local>
2023-05-11 15:56:57 -0400

    Adding calc caculator

--- base.py
+++ base.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route('/calinput', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        print(request)
+    else:
+        return jsonify({"test": "hello_world"})
